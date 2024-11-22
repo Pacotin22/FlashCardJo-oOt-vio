1e1f1d691fe4182cee143b06bad8b3622481e0fa
Pacotin22 <163549883+Pacotin22@users.noreply.github.com>
2024-11-22 11:52:52 -0300

    Add files via upload
    COMEÇO

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+'EMPREENDEDORISMO',
+'Como se tornar um EMPREENDEDOR?',
+'Para se tornar um empreendedor de sucesso, é preciso ter habilidades como criatividade, capacidade de inovação, liderança, proatividade e perseverança.'
+)
+
+criaCartao(
+'MARKETING DIGITAL',
+'O que é COPY?',
+'Copy é uma abreviação do Copywriting, que no marketing digital se refere a textos que propõem convencer e persuadir o leitor a realizar uma ação.'
+)
+
+criaCartao(
+'EMPREENDEDORISMO',
+'Como saber ESCALAR uma empresa',
+'Modelo de Negócio Repetível, Mercado com Potencial, Estrutura de Custos Favorável, Equipe e Liderança,  Modelo de Monetização e Rentabilidade, Canais de Distribuição Escaláveis.'
+)
+
+criaCartao(
+'MARKETING DIGITAL',
+'O que é um FÚNIL de vendas?',
+ 'O funil de vendas de marketing digital é uma estratégia que orienta os clientes ao longo de sua jornada de consumidor desde o topo até o fundo do funil. Criar uma sequência orientada de ações com estímulos de conteúdos relevantes é o que faz com que os leads se movimentem de uma etapa para outra.'
+)
